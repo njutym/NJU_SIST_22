b1a95676eeee82727b4605df0389f65df38c535e
田永铭(Yong-Ming Tian) <139630142+njutym@users.noreply.github.com>
2024-09-22 23:09:27 +0800

    Delete freshman_first_semester/期末作业/（已完成）（要懂得拼搏！）c语言project1 directory

--- freshman_first_semester/期末作业/（已完成）（要懂得拼搏！）c语言project1/痛苦的过程/测试字符串.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char a = 's8888';
-	printf("%c",a);
-}
--- freshman_first_semester/期末作业/（已完成）（要懂得拼搏！）c语言project1/痛苦的过程/连续赋值.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	int a,b,c;
-	b = 6;
-	a = b = 3;
-	printf("%d %d",a,b);
-}
